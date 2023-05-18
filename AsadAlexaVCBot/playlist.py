c4c6af84a74d957f067ae00396e4f5855c054218
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-18 20:40:00 +0530

    Update playlist.py

--- AsadAlexaVCBot/playlist.py
+++ AsadAlexaVCBot/playlist.py
@@ -22,11 +22,11 @@ async def playlist(client, m: Message):
                     [
                         [
                             InlineKeyboardButton(
-                                "❤️ ᴏᴡɴᴇʀ ❤️", url=f"https://t.me/Dr_Asad_Ali"
+                                "❤️ ᴏᴡɴᴇʀ ❤️", url=f"https://t.me/ALONE_WAS_BOT"
                             ),
                             InlineKeyboardButton(
                                 "👨‍‍👧‍👦 ɢʀᴏᴜᴘ 👨‍👧‍👦",
-                                url=f"https://t.me/Shayri_Music_Lovers",
+                                url=f"https://t.me/AlonesHeaven",
                             ),
                         ]
                     ]
